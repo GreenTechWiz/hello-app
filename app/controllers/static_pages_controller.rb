cc0d8f819bf5132e767ecd1c26d61327bc591bea
GreenTechWiz <aggreen0405@gmail.com>
2019-03-05 20:45:49 -0600

    Add routes and static pages contorller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
 class StaticPagesController < ApplicationController
   #Methods are controller actions in Rails
   # Web request arrives at hellwow action in static_pages controller. Controller actions are just methods in controllers
+  # This hello method calls the hello.html.erb file
   def hello
-    render "hello"
+    # render 'hello.html.erb'
   end
+
+  def home   # Helps sending static pages to home page
+    
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 # User sends web request to /hello_world and the routes file sends it to the hello actions in the static pages controller
 # => sends the gets to
-get '/hello_world' => 'static_pages#hello'
-
+  get '/hello_world' => 'static_pages#hello'
+  get '/' => 'static_pages#home'
+  get '/home' => 'static_pages#home'
 end
